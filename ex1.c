5e6c0f6e8c9631c211357fddf6c6d2fa91929625
jpch89 <jpch89@outlook.com>
2019-06-18 18:07:26 +0800

    add new book

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* This is a commet. */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+
+    // this is also a comment
+    print("You are %d miles away.\n", distance)
+
+    return 0;
+}
+
