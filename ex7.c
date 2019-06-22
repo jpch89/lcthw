6acc6dc94218cc6e9b904a64c0a1bcf03c55cac9
进击的团子 <jpch89@outlook.com>
2019-06-22 23:25:41 +0800

    add ex10.c

--- ex7.c
+++ ex7.c
@@ -27,8 +27,14 @@ int main(int argc, char * argv[])
     long universe_of_detects = 1L * 1024L * 1024L * 1024L;
     printf("The entire universe has %ld bugs.\n", universe_of_detects);
     
+    double expected_bugs = bugs * bug_rate;
+    printf("You are expected to have %f bugs.\n", expected_bugs);
+
+    double part_of_universe = expected_bugs / universe_of_detects;
+    printf("That is only a %e portion of the universe.\n", part_of_universe);
+
     // this makes no sense, just a demo of something weird
-    char nul_byte = '\n';
+    char nul_byte = '\0';
     int care_percentage = bugs * nul_byte;
     printf("Which means you should care %d%%.\n", care_percentage);
     
